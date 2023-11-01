eae46700fd5a4e2f8416668b9c777233cf4a9918
kacijcox <145575906+kacijcox@users.noreply.github.com>
2023-11-01 23:34:20 +0000

    First commmit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("I can't make it tonight.");
+
+  document.getElementById("generate").addEventListener("click", function() {
+    document.getElementById("excuse").innerText = generateExcuse();
+  });
+};
+
+const generateExcuse = () => {
+  let who = ["My house", "My lizard", "The world", "My sanity"];
+  let action = ["broke", "flew away", "crushed", "broke"];
+  let what = ["my car", "with my wallet", "my computer"];
+  let when = [
+    "this morning",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was walking"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
